a64442daa2fc1a51a8f8fb68eaee22669c055803
David Calderon <calderon.djorge@gmail.com>
2018-12-13 01:49:26 +0000

    tutorial 1002 mila2

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 'use strict';
 var express = require("express"),
 app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+// // app.use(express.static(__dirname + "/public"));
 
 // We’re setting up an extremely simple server here.
 const http = require('http');
