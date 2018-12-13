062b7c5e2422844bbeca983f7ce2fe27e80005e1
David Calderon <calderon.djorge@gmail.com>
2018-12-13 04:33:58 +0000

    cambios de diseño

--- app.js
+++ app.js
@@ -19,21 +19,21 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
-app.get("/en", function(req, res){
-    res.render("landing-en");
+app.get("/entra-al-mundo-de-los-espiritus", function(req, res){
+    res.render("entraAlMundo");
 });
 
-app.get("/es", function(req, res){
-    res.render("landing-es");
+app.get("/manko-y-el-mundo-de-los-espiritus", function(req, res){
+    res.render("libroManko");
 });
 
-app.get("/pago_exitoso", function(req, res){
-    res.render("pago-exitoso");
-});
+// app.get("/pago_exitoso", function(req, res){
+//     res.render("pago-exitoso");
+// });
 
-app.get("/pago_en_proceso", function(req, res){
-    res.render("pago-en-proceso");
-});
+// app.get("/pago_en_proceso", function(req, res){
+//     res.render("pago-en-proceso");
+// });
 
 app.get("/client_info", function(req, res){
     res.render("client-info");
@@ -48,7 +48,7 @@ app.post("/client_info", function(req, res){
     var mailData = {
           from: 'Registros de Temazcal <servidor@temazcal.info>',
           to: 'hola@temazcal.info',
-          subject: 'Registro en temazcal.info',
+          subject: 'Registro en MANKO',
           text: 'Un nuevo registro: \n\n' +
           name + '\n\n' + email + '\n\n' + phone +
           '\n'
@@ -66,71 +66,5 @@ app.get("*", function(req, res){
 });
 
 app.listen(port, ip, function(){
-    console.log("Servidor de temazcal inicado");
+    console.log("Manko y el mundo de los espiritus, servidor iniciado");
 });
-
-
-// 'use strict';
-// var express = require("express"),
-// app = express();
-
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(__dirname + '/public'));
-// app.set("view engine", "ejs");
-// //http
-// const http = require('http');
-
-// var data = require("./data");
-// var api_key = data.mailKey;
-// var domain =  data.mailUser;
-// var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
-
-// // These could (should) be set as env vars.
-// const port = process.env.PORT || 8000;
-// const host = process.env.HOST || 'localhost';
-
-// //---------rutas
-// app.get("/", function(req, res){
-//     res.render("index");
-// });
-
-// app.get("/es", function(req, res){
-//     res.render("landing-es");
-// });
-
-// app.get("*", function(req, res){
-//     res.send("¡412!¡Tenemos un 412! ");
-// });
-
-// app.get("/client_info", function(req, res){
-//     res.render("client-info");
-// });
-
-// app.post("/client_info", function(req, res){
-//     var name = req.body.name;
-//     var email = req.body.email;
-//     var phone = req.body.phone;
-//     console.log(email + name + phone);
-    
-//     var mailData = {
-//           from: 'Registros de Manko <servidor@manko.app>',
-//           to: 'hola@manko.app',
-//           subject: 'Registro en Manko.app',
-//           text: 'Un nuevo registro: \n\n' +
-//           name + '\n\n' + email + '\n\n' + phone +
-//           '\n'
-//         };
-        
-//         mailgun.messages().send(mailData, function (error, body) {
-//           console.log(body);
-//         });
-    
-//     res.render("client-info");
-// });
-
-// //----------------rutas
-
-// http.createServer(app).listen(port, host);
-// console.log('Mundo de los espíritus corriendo en http://${host}:${port}');
-
